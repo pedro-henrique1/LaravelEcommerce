dda1bb7efeca8f841e59eb0f23e4dca67ef9464d
pedro <ph8464803@gmail.com>
2021-06-14 23:14:29 -0300

    style: adicionado products categories model e component

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Category;
+use App\Models\HomeCategory;
 use App\Models\HomeSlide;
 use App\Models\Product;
 use Livewire\Component;
@@ -13,7 +15,16 @@ class HomeComponent extends Component
     {
         $sliders = HomeSlide::where('status', 1)->get();
         $lproducts = Product::orderBy('created_at', 'DESC')->take(8)->get();
-        return view('livewire.home-component', ['sliders' => $sliders, 'lproducts' => $lproducts])->layout(
+        $category = HomeCategory::find(1);
+        $cats = explode(',', $category->sel_categories);
+        $categories = Category::whereIn('id', $cats)->get();
+        $no_of_products = $category->no_of_products;
+        return view('livewire.home-component', [
+            'sliders' => $sliders,
+            'lproducts' => $lproducts,
+            'categories' => $categories,
+            'no_of_products' => $no_of_products
+        ])->layout(
             'layouts.base'
         );
     }
