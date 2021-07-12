7c3a7479a3cc51a4e8c8bf48271bc5363a6621cc
pedro <ph8464803@gmail.com>
2021-07-12 18:46:48 -0300

    style: :construction:

--- resources/views/livewire/admin/admin-home-category-component.blade.php
+++ resources/views/livewire/admin/admin-home-category-component.blade.php
@@ -2,7 +2,7 @@
     <div class="container" style="padding: 30px 0;">
         <div class="row">
             <div class="col-md-12">
-            </div class="panel panel-default">
+            <div class="panel panel-default">
             <div class="panel-heading">
                 Manager Home Categories
             </div>
