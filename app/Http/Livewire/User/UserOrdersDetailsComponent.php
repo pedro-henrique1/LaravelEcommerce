2b5d3023571e1ca7b510b7aafd5fe991276d1382
pedro <ph8464803@gmail.com>
2021-07-20 15:00:01 -0300

    style: cancelamento da comprar

--- app/Http/Livewire/User/UserOrdersDetailsComponent.php
+++ app/Http/Livewire/User/UserOrdersDetailsComponent.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire\User;
 
 use App\Models\Order;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class UserOrdersDetailsComponent extends Component
@@ -13,6 +14,15 @@ class UserOrdersDetailsComponent extends Component
     public function mount($order_id)
     {
         $this->order_id = $order_id;
+    }
+
+    public function canceledOrder()
+    {
+        $order = Order::find($this->order_id);
+        $order->status = 'canceled';
+        $order->canceled_date = DB::raw('CURRENT_DATE');
+        $order->save();
+        session()->flash('order_message', 'Order has been canceled');
     }
 
     public function render()
--- resources/views/livewire/user/user-orders-details-component.blade.php
+++ resources/views/livewire/user/user-orders-details-component.blade.php
@@ -1,18 +1,53 @@
 <div>
     <div class="container-fluid" style="margin: 1.5rem 0 1rem 0;">
+
+        <div class="row">
+            <div class="col-md-12">
+                @if(Session::has('order_message'))
+                    <div class="alert alert-success" role="alert">{{Session::get('order_message')}}</div>
+                @endif
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-md-6">
+                                Ordered Details
+                            </div>
+                            <div class="col-md-6">
+                                <a href="{{route('user.orders')}}" class="btn btn-success pull-right">My Orders</a>
+                                @if($order->status == 'ordered')
+                                    <a href="#" wire:click.prevent="canceledOrder" class="btn btn-warning pull-right">Cancel
+                                        Order</a>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <table class="table">
+                            <th>Order Id</th>
+                            <td>{{$order->id}}</td>
+                            <th>Order Date</th>
+                            <td>{{$order->created_at}}</td>
+                            <th>Status</th>
+                            <td>{{$order->status}}</td>
+                            @if($order->status == 'delivered')
+                                <th>Delivered Date</th>
+                                <td>{{$order->delivered}}</td>
+                            @elseif($order->status == 'canceled')
+                                <th>Canceled Date</th>
+                                <td>{{$order->canceled_date}}</td>
+                            @endif
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <!-- Ordered Items -->
         <div class="row">
             <div class='col-md-12'>
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        <div class="row">
-                            <div class="col-md-6">
-                                Ordered Items
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <a href="{{route('user.orders')}}" class="btn btn-success pull-right">My Orders</a>
-                        </div>
+                        Orders Items
                     </div>
                     <div class='panel-body'>
                         <div class="wrap-iten-in-cart">
