be41be06124f5e203485cd2e28cb02b502dfe767
pedro <ph8464803@gmail.com>
2021-07-24 22:49:51 -0300

    chore: :construction_worker: update no arquivo (DatabaseSeeder)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,8 @@ use App\Models\HomeCategory;
 use App\Models\Product;
 use App\Models\Sale;
 use App\Models\User;
+use Database\Factories\CouponFactory;
+use Database\Factories\SlideFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,10 +20,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Category::factory(6)->create();
-        Product::factory(6)->create();
-        User::factory(1)->create();
-        HomeCategory::factory(5)->create();
-        Sale::factory(1)->create();
+        Category::factory()->count(5)->create();
+        Product::factory()->count(10)->create();
+        User::factory()->count(1)->create();
+        HomeCategory::factory()->count(5)->create();
+        Sale::factory()->count(1)->create();
+        CouponFactory::factory()->count(1)->create();
+        SlideFactory::factory()->count(1)->create();
     }
 }
