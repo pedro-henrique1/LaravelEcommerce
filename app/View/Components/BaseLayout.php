c261d053ee89321eca5972885af8eb11d37dd5aa
pedro <ph8464803@gmail.com>
2021-07-30 23:07:12 -0300

    fix: update bug no erro de logib

--- app/View/Components/BaseLayout.php
+++ app/View/Components/BaseLayout.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class GuestLayout extends Component
+class BaseLayout extends Component
 {
     /**
      * Get the view / contents that represents the component.
